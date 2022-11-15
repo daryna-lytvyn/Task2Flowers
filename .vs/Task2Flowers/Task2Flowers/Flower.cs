b58068fe162b759038681730109bb0e4258301ff
Дарина Литвин <daryna.lytvyn@nure.ua>
2022-11-15 22:43:53 +0200

    Created class flower

--- .vs/Task2Flowers/Task2Flowers/Flower.cs
+++ .vs/Task2Flowers/Task2Flowers/Flower.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task2Flowers
 {
+
+
     class Flower
     {
+        private int id;
+
+        private string kind;
+
+        private string variety;
+
+        private Color color;
+
+        public Flower(int id, string kind, string variety, Color color)
+        {
+            this.id = id;
+            this.kind = kind;
+            this.variety = variety;
+            this.color = color;
+        }
     }
 }
--- .vs/Task2Flowers/Task2Flowers/Program.cs
+++ .vs/Task2Flowers/Task2Flowers/Program.cs
@@ -4,9 +4,6 @@ namespace Task2Flowers
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+ 
     }
 }
